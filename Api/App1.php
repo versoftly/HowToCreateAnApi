da1501e12a2982d71e669b3701abd46c202b25af
ramiro <mostlyphpsoftware@gmail.com>
2024-02-16 17:20:54 -0700

    section 2 , css grid 2 columns equal

--- Api/App1.php
+++ Api/App1.php
@@ -14,6 +14,10 @@
 
     require_once ( "../../Libreria/ParaLaVista/Functions/TwoEcolsFlex/TwoEColsFlex.php" );
 
+    require_once ( "../../Libreria/ParaLaVista/Functions/Normalize/Normalize.php" );
+
+    require_once ( "../../Libreria/ParaLaVista/Functions/TwoEqualColumns/TwoEqualColumns.php" );
+
     $obj1 = new Content;
 
     $obj2 = new Adapto;
@@ -65,9 +69,34 @@
 
     $section2 = Element::OHtml ( "section" , 
 
-        "Section 2" , 
+        Element::OHtml ( "div" ,
 
-    '' , 1 );
+            Element::OHtml ( "h2" , "About" , '' , 1 ) .
+
+            Element::OHtml ( "p" , "
+                Dedicated PHP developer, passionate about developing 
+                sophisticated backend solutions. 
+                I excel in problem-solving and aspire to be an elite backend 
+                engineer, committed to enhancing user experiences through 
+                seamless, innovative software. My goal is to deliver 
+                high-quality, efficient, and intuitive applications 
+                that meet the evolving needs of users.
+            " , '' , 1 )
+        
+        , '' , 1) .
+
+        Element::OHtml ( "div" ,
+
+            Element::OHtml ( "img" , '' , '
+                src="http://adaptowebs.test/v1/Vista/Media/Logo.gif" alt="My Website Logo" 
+                width="100%" style="border-radius: 100%;"
+            ' ) 
+        
+        , '' , 1) . 
+
+        Element::OHtml ( "h2" , "Skills" , '' , 1 )
+
+    , 'class="TwoEqualColumns" style="padding:15px;"' , 1 );
 
     $section3 = Element::OHtml ( "section" , 
 
@@ -82,7 +111,8 @@
     '' , 1 );
 
     $style = $obj2 -> styleCss ( 
-        colorsRoot () . baseCss () . TwoEColsFlex ()
+        colorsRoot () . baseCss () . TwoEColsFlex () . Normalize () . 
+        TwoEqualColumns ()
     );
 
     page ( 
---
+++ Libreria/ParaLaVista/Functions/TwoEqualColumns/TwoEqualColumns.php
@@ -0,0 +1,25 @@
+<?php
+
+function TwoEqualColumns () {
+
+    return '
+    
+        @media ( min-width: 768px ) {
+                    
+            .TwoEqualColumns {
+                
+                display: grid;
+                grid-template-columns: repeat(2 , 50%);
+                column-gap: 1rem;
+                align-items: center;
+                background-color: var(--gris);
+                
+            }
+            
+        }
+    
+    ';
+
+}
+
+?>
